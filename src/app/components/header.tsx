ac5fbec8307800f092967d02eb706c2283126094
mafyyu <morita0126m@gmail.com>
2025-02-10 17:45:56 +0900

    文字をPropsで渡せるように #2

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,12 +1,13 @@
 interface HeaderProps{
     color:string
+    text:string
 }
 
 
-export default function Header({color}: HeaderProps){
+export default function Header({color,text}: HeaderProps){
     return(
         <div className={`${color} text-white w-full h-[100px] text-6xl flex justify-center items-center`}>
-        Home
+        {text}
         </div>
     );
 }
