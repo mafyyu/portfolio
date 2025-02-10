a1e0f328ad9c6117955b90905546762bb13fb580
mafyyu <morita0126m@gmail.com>
2025-02-10 18:14:15 +0900

    フォントサイズ変更 #2

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps{
 
 export default function Header({color,text}: HeaderProps){
     return(
-        <div className={`${color} text-white w-full h-[100px] text-6xl flex justify-center items-center`}>
+        <div className={`${color} text-black w-full h-[100px] text-4xl flex justify-center items-center font-Orbitron`}>
         {text}
         </div>
     );
