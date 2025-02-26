afaa3faaf7e35aee9bbb893bd3cb3c183d4a7cb0
mafyyu <morita0126m@gmail.com>
2025-02-26 09:30:51 +0900

    引数の指定の修正 #4

--- src/app/components/tab.tsx
+++ src/app/components/tab.tsx
@@ -1,29 +1,33 @@
-interface TabProps{
-    color:string,
-    hsize:number,
-    wsize:number,
-    text:string,
-    link:string
-}
-
-export default function Tab({color,hsize,wsize,text,link}:TabProps){
-    return(
-        <a href={link}>
-            <div className="w-full h-full max-w-3xl mx-auto">
-                <div className={`${color} rounded-2xl shadow-lg h-[${hsize}px] w-[${wsize}px]`}>
-                {/* Window Controls */}
-                    <div className="bg-white px-4 py-3 flex items-center gap-2">
-                        <div className="w-3 h-3 rounded-full bg-[#FF5F56]" />
-                        <div className="w-3 h-3 rounded-full bg-[#FFBD2E]" />
-                        <div className="w-3 h-3 rounded-full bg-[#27C93F]" />
-                    </div>
-        
-                {/* Content */}
-                    <div className="px-6 pb-8 font-ZenakuGothicNew text-4xl font-black flex justify-center items-center h-full">
-                        <p className="font-ZenakuGothicNew text-4xl font-black">{text}</p>
-                    </div>
-                </div>
+interface TabProps {
+    color: string;
+    hsize: number;
+    wsize: number;
+    text: string;
+    link: string;
+  }
+  
+  export default function Tab({ color, hsize, wsize, text, link }: TabProps) {
+    return (
+      <a href={link}>
+        <div className="w-full h-full max-w-3xl mx-auto">
+          <div
+            className={`${color} rounded-2xl shadow-lg`}
+            style={{ height: `${hsize}px`, width: `${wsize}px` }}
+          >
+            {/* Window Controls */}
+            <div className="bg-white px-4 py-3 flex items-center gap-2">
+              <div className="w-3 h-3 rounded-full bg-[#FF5F56]" />
+              <div className="w-3 h-3 rounded-full bg-[#FFBD2E]" />
+              <div className="w-3 h-3 rounded-full bg-[#27C93F]" />
+            </div>
+  
+            {/* Content */}
+            <div className="px-6 pb-8 font-ZenakuGothicNew text-4xl font-black flex justify-center items-center h-full">
+              <p className="font-ZenakuGothicNew text-4xl font-black">{text}</p>
+            </div>
+          </div>
         </div>
       </a>
-        );
-}
+    );
+  }
+  
