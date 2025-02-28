008164dcd665b2f53543dcfa7e78fa550df8ac17
mafyyu <morita0126m@gmail.com>
2025-02-28 09:37:41 +0900

    作成中の画面の追加

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,17 +1,17 @@
 import Hamburger from "../components/hamburger";
 import Header from "../components/header";
 
-export default function works() {
+export default function Articles() {
   return (
     <div>
       <div>
-        <Header color="bg-[#FA904F]" text="Works"/>
-        <Hamburger/>
+        <Header color="bg-[#FFA1A3]" text="Articles" />
+        <Hamburger />
       </div>
       <div className="flex flex-col justify-center items-center">
         <div className="text-4xl font-NotoSans">🚀Under Construction🚀</div>
         <div className="text-4xl font-NotoSans">このページは作成中です</div>
-      </div> 
-    </div> 
+      </div>
+    </div>
   );
 }
--- src/app/updates/page.tsx
+++ src/app/updates/page.tsx
@@ -1,6 +1,17 @@
+import Hamburger from "../components/hamburger";
+import Header from "../components/header";
+
 export default function updates() {
     return (
-      <h1>updatespage</h1>
+    <div>
+      <div>
+        <Header color="bg-[#FA904F]" text="Updates"/>
+        <Hamburger/>
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <div className="text-4xl font-NotoSans">🚀Under Construction🚀</div>
+        <div className="text-4xl font-NotoSans">このページは作成中です</div>
+      </div>  
+    </div>
     );
-  }
-  
+}
