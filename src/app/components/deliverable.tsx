9fd374f52b4cd9eff859f27b4ac91fbcc3504f48
mafyyu <morita0126m@gmail.com>
2025-03-06 16:55:47 +0900

    投稿周りの修正

--- src/app/components/deliverable.tsx
+++ src/app/components/deliverable.tsx
@@ -1,24 +1,24 @@
-export default function Deliverable(){
-    return(
-        <div className="my-10 w-full h-full flex justify-center items-center">
-            <div className="w-[75%] h-[40%] border border-[#FA904F]">
-                <div className="flex flex-row justify-start items-center gap-x-[15%] pb-4 w-full h-1/3 ml-[15%] my-10 rounded-lg">
-                    <img src="/x.png" alt="" className="w-[15%] h-[15%] object-contain"/>
-                    <div className="flex flex-col space-y-3">
-                        <div className="font-NotoSans text-3xl flex items-center">My Map</div>
-                        <a href="https://beginners-hack-new.vercel.app/" className="font-NotoSans text-sm">https://beginners-hack-new.vercel.app/</a>
-                    </div>
+export default function Deliverable({ data }: { data: any[] }) {
+    return (
+      <div className="my-10 w-full h-full flex flex-col justify-center items-center space-y-[2%]">
+        {data.map((item, index) => (
+          <a href={item.data.url} key={index} className="w-full flex justify-center items-center">
+            <div className="w-[75%] h-[auto] border border-gray-200 rounded-lg overflow-hidden shadow-md hover:shadow-lg transition-all duration-300 hover:scale-102">
+              <div className="flex flex-row justify-start items-center gap-x-[10%] p-4 bg-white">
+                <img
+                  src={`/workicon/${item.data.image}`}
+                  alt={item.data.title}
+                  className="w-[150px] h-[150px] object-contain border-2 border-gray-100 shadow-sm"
+                />
+                <div className="flex flex-col space-y-2">
+                  <div className="font-NotoSans text-3xl text-gray-800">{item.data.title}</div>
+                  <div className="font-NotoSans text-sm text-gray-600">{item.content}</div>
                 </div>
-                <div className="flex flex-row justify-start items-center gap-x-[15%]">
-                    <div className="w-[15%] flex flex-wrap ml-[15%] gap-4 mb-3">
-                        <img src="/x.png" alt="" className="w-9 h-9" />
-                        <img src="/x.png" alt="" className="w-9 h-9" />
-                        <img src="/x.png" alt="" className="w-9 h-9" />
-                        <img src="/x.png" alt="" className="w-9 h-9" />
-                    </div>
-                    <div className="flex flex-wrap font-NotoSans text-sm w-[45%]">これはKC3Hack2025で作成したものです。これはKC3Hack2025で作成したものです。これはKC3Hack2025で作成したものです。</div>
-                </div>
+              </div>
             </div>
-        </div>    
-    )
-}
+          </a>
+        ))}
+      </div>
+    );
+  }
+  
